ddd74966ddc6d6d9a64fd79fb6ebacbec8acf5d4
Sat Palshetkar <sat.palshetkar2@gmail.com>
2025-06-26 16:04:13 -0400

    Time: 0 ms (100.00%) | Memory: 43 MB (87.48%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -44,3 +44,10 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+/*
+Intuition:
+1. Find the first dip from right to left as the next permutation will be the next biggest number right after the current one (int[] nums)
+2. Swap the dip number (index) with the number which is the immediate larger number to it
+3. After the swap all the digits after the index will be in the descending order from left to right so we need to reverse that into ascending order
+*/
